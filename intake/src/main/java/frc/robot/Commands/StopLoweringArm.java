fdd8fd53d4339e7f4725d130515964c37ecdc876
Sigal Minsky-Primus <snigbot@gmail.com>
2021-12-06 16:56:25 -0500

    _

--- intake/src/main/java/frc/robot/Commands/StopLoweringArm.java
+++ intake/src/main/java/frc/robot/Commands/StopLoweringArm.java
@@ -1,13 +1,7 @@
 package frc.robot;
 import frc.robot.Commands;
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.command.Command;
-
-
-=======
 import frc.robot.Subsystems.intake;
 
->>>>>>> 05e21bc5c7a29a3a37a401106dcc2e72f0b500ab
 public class StopLoweringArm extends Command {
 
     public StopLoweringArm() {}
@@ -19,11 +13,6 @@ public class StopLoweringArm extends Command {
 
     @Override
     public boolean isFinished() {
-        return true;
-    }
-    
-    @Override
-    public boolean isFinished(){
         return true;
     }
 }
